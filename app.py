03c1deb948de5531150939da48e57cf496c2477a
yoyo10231224 <xuyousheng27@gmail.com>
2026-01-30 21:06:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from supabase import create_client
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def home():
-    return "Backend is running"
+supabase = create_client(
+    os.environ["SUPABASE_URL"],
+    os.environ["SUPABASE_KEY"]
+)
+
 
 @app.route("/api/hello")
 def hello():
+    print(555)
     return jsonify({"msg": "Hello Render"})
+    
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/api/send", methods=["POST"])
+def send():
+    data = request.json
+    supabase.table("chat").insert({
+        "name": data["name"],
+        "text": data["text"]
+    }).execute()
+    return jsonify({"ok": True})
+
+@app.route("/api/list")
+def list_msg():
+    res = supabase.table("chat").select("*").order("id").execute()
+    return jsonify(res.data)
